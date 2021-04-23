7bbe1c50c1066dfdf17bfb5a3163f39480e02303
selmasuicmez <selmasuicmez@gmail.com>
2021-04-23 15:36:20 +0200

    basis code ovapp 23/04/2021

---
+++ src/Location.java
@@ -0,0 +1,16 @@
+public class Location {
+    String name;
+
+    public Location(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
