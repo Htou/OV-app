8771d2d1c86945b7d60693d3fe9ae082eacd76d5
ThijmenGeurtsen <thijmen01g@gmail.com>
2021-04-26 20:51:06 +0200

    added: a list of a trajectory which contains multiple stations.

--- src/RouteHandler.java
+++
@@ -1,6 +0,0 @@
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-
-public class RouteHandler {
-   private ArrayList<ArrayList> trajectList = new ArrayList();
-}
---
+++ src/TrajectoryHandler.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class TrajectoryHandler {
+   private ArrayList<Trajectory> trajectoryList = new ArrayList();
+
+
+   public ArrayList<Trajectory> loadTrajectory(){
+      ArrayList<Station> stationUtrechtToAmsterdam = new ArrayList();
+
+      stationUtrechtToAmsterdam.add(new Station("Utrecht",3 ,2.04));
+      stationUtrechtToAmsterdam.add(new Station("Utrecht Zuilen",5 ,5.29));
+      stationUtrechtToAmsterdam.add(new Station("Maarssen",4 ,4.83));
+      stationUtrechtToAmsterdam.add(new Station("Breukelen",10 ,12.1));
+      stationUtrechtToAmsterdam.add(new Station("Abcoude",0 ,0));       //end station
+      Trajectory trajectoryUtrechtToAmsterdam = new Trajectory(stationUtrechtToAmsterdam);
+
+      trajectoryList.add(trajectoryUtrechtToAmsterdam);
+
+      return trajectoryList;
+   }
+
+
+}
