b1516ef4dd2aee7b451d5c252180b29d084f1b3f
Hichem Touati <hichemtouati@outlook.com>
2021-04-26 18:20:53 +0200

    rename traject to trajectory, its english counterpart; Added atributes getters and setters

--- src/Trajectory.java
+++ src/Trajectory.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
 
 public class Trajectory {
-    private ArrayList<Station> stationList = new ArrayList();
+    private ArrayList<Station> stationList;
+
+    public Trajectory(ArrayList<Station> stationList ) {
+        this.stationList = stationList;
+    }
+    public ArrayList<Station> getStationList(){
+        return stationList;
+    }
+    public void setStationList(ArrayList<Station>newStationList ){
+        this.stationList = newStationList;
+    }
 }
