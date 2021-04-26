5a6d3344b4c15bc563e50181bf2102dcad1d6c5c
ThijmenGeurtsen <thijmen01g@gmail.com>
2021-04-26 20:55:22 +0200

    added something for the future so we can impliment the pathfinding algorithm easier and to calculate the time for the trajectory's.
    the value increments to see how often the train departs.
    
    Added a firstDapartureTime.

--- src/Trajectory.java
+++ src/Trajectory.java
@@ -2,13 +2,22 @@ import java.util.ArrayList;
 
 public class Trajectory {
     private ArrayList<Station> stationList;
+    private int increments; // is how often the first train goes from the first station for example: 30
+
+    //each half hour or 15 minutes the train departures so we need the time when it first departs so the time can be calculated further
+    int firstDepartureTime;
+
 
     public Trajectory(ArrayList<Station> stationList ) {
         this.stationList = stationList;
+        this.increments = increments;
+        this.firstDepatureTime = firstDepatureTime;
     }
+
     public ArrayList<Station> getStationList(){
         return stationList;
     }
+
     public void setStationList(ArrayList<Station>newStationList ){
         this.stationList = newStationList;
     }
