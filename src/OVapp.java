b50be07a41e6a05a19ce639ec70a299a8c42e9f8
ThijmenGeurtsen <thijmen01g@gmail.com>
2021-04-27 14:47:22 +0200

    added a locationB variable and added a getter for this

--- src/OVapp.java
+++ src/OVapp.java
@@ -1,13 +1,33 @@
 public class OVapp {
     private TrajectoryHandler trajectoryList;
     private double distanceToLocationB;
+    private String locationB;
 
     public OVapp(){
         trajectoryList = new TrajectoryHandler();
         trajectoryList.loadTrajectory();
 
 
+        locationB = "Utrecht Centraal";
+
+
     }
+
+    ///////////////////////////////////////
+    ///      setters and getters        ///
+    ///////////////////////////////////////
+    public double getDistanceToLocationB(){
+        return distanceToLocationB;
+    }
+
+
+    public String getLocationB(){
+        return locationB;
+    }
+    public void setLocationB(String newLocationB){
+        locationB = newLocationB;
+    }
+
 
     public void runProgram() {
 
@@ -15,11 +35,8 @@ public class OVapp {
         gui.startGui();
 
     }
-    public double getDistanceToLocationB(){
-        return distanceToLocationB;
-    }
 
-    public double calcDistanceToStation(String station){
+    public void calcDistanceToStation(String station){
         Trajectory utrechtToAmsterdam = trajectoryList.getTrajectory(0); // there is only one trajectory atm so it starts at Utrecht.
 
 
@@ -28,17 +45,20 @@ public class OVapp {
 
         int index = utrechtToAmsterdam.indexOf(station);
         if (index == -1){
+            distanceToLocationB = -1;
 
-            return -1;
+
+        } else {
+            //we start at 0 so the program can just go through the list and add the distances
+            double totalDistance = 0.0;
+            for (int i = 0; i < index; i++) {
+                totalDistance = totalDistance + utrechtToAmsterdam.getDistanceToNextStation(index);
+            }
+
+            distanceToLocationB = totalDistance;
+
         }
 
-        double totalDistance = 0.0;
-        for (int i=0; i<index; i++){
-            totalDistance = totalDistance + utrechtToAmsterdam.getDistanceToNextStation(index);
-        }
-
-        //we start at 0 so the program can just go through the list and add the distances
-        return totalDistance;
 
 
 
