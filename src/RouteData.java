e74b11cd7f5d34c6636144d77c456ab3c1e55024
Hichem Touati <hichemtouati@outlook.com>
2021-04-27 18:42:20 +0200

    Created data class for locationA, locationB, distanceFromAToB.

---
+++ src/RouteData.java
@@ -0,0 +1,33 @@
+public class RouteData {
+    private String locationA;
+    private String locationB;
+    private double distanceFromAToB;
+
+
+
+    public String getLocationA() {
+        return locationA;
+    }
+    public void setLocationA(String locationA){
+        this.locationA = locationA;
+    }
+
+    public String getLocationB() {
+        return locationB;
+    }
+    public void setLocationB(String locationB){
+        this.locationB = locationB;
+    }
+
+    public double getDistanceFromAToB() {
+        return distanceFromAToB;
+    }
+
+    public void setDistanceFromAToB(double distanceFromAToB){
+        this.distanceFromAToB = distanceFromAToB;
+    }
+
+
+
+
+}
