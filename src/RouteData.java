3ec19c20b70afe3f8cecf790c15e2e5ed2e549a9
ThijmenGeurtsen <thijmen01g@gmail.com>
2021-04-28 14:20:38 +0200

    added localTime.

--- src/RouteData.java
+++ src/RouteData.java
@@ -1,8 +1,27 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class RouteData {
     private String locationA;
     private String locationB;
     private double distanceFromAToB;
+    private static LocalTime time;
 
+    public RouteData(){
+        String str = "00:00";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        time = LocalTime.parse(str, formatter);
+    }
+
+
+
+    public void addMinutesTime(int minutes){
+        time = time.plusMinutes(minutes);
+    }
+    public LocalTime getTime(){
+        return time;
+    }
 
 
     public String getLocationA() {
@@ -22,9 +41,11 @@ public class RouteData {
     public double getDistanceFromAToB() {
         return distanceFromAToB;
     }
-
     public void setDistanceFromAToB(double distanceFromAToB){
+
         this.distanceFromAToB = distanceFromAToB;
+
+
     }
 
 
