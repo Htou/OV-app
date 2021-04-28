3657f8a4cf7fada1fdf9211b6e47f9d5030e19d4
ThijmenGeurtsen <thijmen01g@gmail.com>
2021-04-28 14:46:42 +0200

    Added te start to generate a list for depatureTimes

--- src/OVapp.java
+++ src/OVapp.java
@@ -1,4 +1,6 @@
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class OVapp {
     private TrajectoryHandler trajectoryList;
@@ -37,9 +39,6 @@ public class OVapp {
         if (index == -1){
             routeData.setDistanceFromAToB(-1);
 
-
-
-
         } else {
             //we start at 0 so the program can just go through the list and add the distances
             double totalDistance = 0.0;
@@ -65,6 +64,21 @@ public class OVapp {
 
 
 
+    }
+
+    public void generateListDepatureTimes(){
+
+        String time = "00:00";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime newTime = LocalTime.parse(time, formatter);
+        int minutes = routeData.getTime().getMinute();
+
+        Trajectory utrechtToAmsterdam = trajectoryList.getTrajectory(0);
+
+        for (int i=0; i<40; i++){
+
+
+        }
     }
 
 
