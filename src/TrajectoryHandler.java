1875622eeb84dfaf654cff77b9b16939a552cd8c
ThijmenGeurtsen <thijmen01g@gmail.com>
2021-05-04 14:51:40 +0200

    placed all trajectorys in one functions. added a vehicleIdentifier for each trjectory.

--- src/TrajectoryHandler.java
+++ src/TrajectoryHandler.java
@@ -12,32 +12,35 @@ public class TrajectoryHandler {
       stationUtrechtToAmsterdam.add(new Station("Maarssen",4 ,4.83));
       stationUtrechtToAmsterdam.add(new Station("Breukelen",10 ,12.1));
       stationUtrechtToAmsterdam.add(new Station("Abcoude",0 ,0));
-      Trajectory trajectoryUtrechtToAmsterdam = new Trajectory(stationUtrechtToAmsterdam,30,20);
+      Trajectory trajectoryUtrechtToAmsterdam = new Trajectory(stationUtrechtToAmsterdam,30,20,"train");
 
+
+      ArrayList<Station> utrechtToAmesfoortBus = new ArrayList();
+
+      utrechtToAmesfoortBus.add(new Station("Utrecht CS",4 ,1.5));
+      utrechtToAmesfoortBus.add(new Station("Vredenburg",1 ,2.4));
+      utrechtToAmesfoortBus.add(new Station("Neude",1 ,0.3));
+      utrechtToAmesfoortBus.add(new Station("Janskerkhof",1 ,1.0));
+      utrechtToAmesfoortBus.add(new Station("Stadsschouwburg",2 ,1.5));
+      utrechtToAmesfoortBus.add(new Station("Wittevrouwen",1 ,2.4));
+      utrechtToAmesfoortBus.add(new Station("Oorsprongpark",2 ,1.7));
+      utrechtToAmesfoortBus.add(new Station("Archimedeslaan",1 ,2.6));
+      utrechtToAmesfoortBus.add(new Station("Steinenburglaan",1 ,0.8));
+      utrechtToAmesfoortBus.add(new Station("Kerklaan, De Bilt",3 ,3.4));
+      utrechtToAmesfoortBus.add(new Station("KNMI, De Bilt",3 ,5.3));
+      utrechtToAmesfoortBus.add(new Station("Amesfoortseweg, De Bilt",0,0)); // end station
+      Trajectory trajectoryUtrechtToAmsterdamBus = new Trajectory(utrechtToAmesfoortBus,30,20,"bus");
+
+
+      trajectoryList.add(trajectoryUtrechtToAmsterdamBus);
       trajectoryList.add(trajectoryUtrechtToAmsterdam);
 
       return trajectoryList;
    }
 
    public ArrayList<Trajectory> loadBusTrajectory(){
-      ArrayList<Station> utrechtToAmesfoort = new ArrayList();
 
-      utrechtToAmesfoort.add(new Station("Utrecht CS",4 ,1.5));
-      utrechtToAmesfoort.add(new Station("Vredenburg",1 ,2.4));
-      utrechtToAmesfoort.add(new Station("Neude",1 ,0.3));
-      utrechtToAmesfoort.add(new Station("Janskerkhof",1 ,1.0));
-      utrechtToAmesfoort.add(new Station("Stadsschouwburg",2 ,1.5));
-      utrechtToAmesfoort.add(new Station("Wittevrouwen",1 ,2.4));
-      utrechtToAmesfoort.add(new Station("Oorsprongpark",2 ,1.7));
-      utrechtToAmesfoort.add(new Station("Archimedeslaan",1 ,2.6));
-      utrechtToAmesfoort.add(new Station("Steinenburglaan",1 ,0.8));
-      utrechtToAmesfoort.add(new Station("Kerklaan, De Bilt",3 ,3.4));
-      utrechtToAmesfoort.add(new Station("KNMI, De Bilt",3 ,5.3));
-      utrechtToAmesfoort.add(new Station("Amesfoortseweg, De Bilt",0,0)); // end station
-      Trajectory trajectoryUtrechtToAmsterdam = new Trajectory(utrechtToAmesfoort,30,20);
 
-      trajectoryList.add(trajectoryUtrechtToAmsterdam);
-      
       return trajectoryList;
    }
 
