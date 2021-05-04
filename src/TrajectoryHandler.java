6753a85c1a4a32d870941e84cc6f76c2bdfb849e
Hichem Touati <hichemtouati@outlook.com>
2021-05-04 17:16:31 +0200

    added new train trajectory

--- src/TrajectoryHandler.java
+++ src/TrajectoryHandler.java
@@ -14,9 +14,30 @@ public class TrajectoryHandler {
       stationUtrechtToAmsterdam.add(new Station("Abcoude",0 ,0));
       Trajectory trajectoryUtrechtToAmsterdam = new Trajectory(stationUtrechtToAmsterdam,30,20,"train");
 
+      ArrayList<Station> stationUtrechtToZwolle = new ArrayList();
+      stationUtrechtToAmsterdam.add(new Station("Utrecht CS",4,4.2));
+      stationUtrechtToAmsterdam.add(new Station("Utrecht Overvecht",5 ,11.1));
+      stationUtrechtToAmsterdam.add(new Station("Bilthoven",3 ,2.9));
+      stationUtrechtToAmsterdam.add(new Station("Den Dolder",9 ,13.5));
+      stationUtrechtToAmsterdam.add(new Station("Amesfoort Centraal",4,11.8));
+      stationUtrechtToAmsterdam.add(new Station("Amesfoort Schothorst",3 ,5.0));
+      stationUtrechtToAmsterdam.add(new Station("Amesfoort Vathorst",5,9.5));
+      stationUtrechtToAmsterdam.add(new Station("Nijkerk",5 ,11.0));
+      stationUtrechtToAmsterdam.add(new Station("Putten",5 ,7.2));
+      stationUtrechtToAmsterdam.add(new Station("Ermelo",4 ,6.6));
+      stationUtrechtToAmsterdam.add(new Station("Hardewijk",8 ,11.7));
+      stationUtrechtToAmsterdam.add(new Station("Nunspeet",6 ,12.0));
+      stationUtrechtToAmsterdam.add(new Station("'t Harde",7 ,11.6));
+      stationUtrechtToAmsterdam.add(new Station("Wezep",7 ,10.5));
+      stationUtrechtToAmsterdam.add(new Station("Zwolle Centraal",0 ,0));
+      Trajectory trajectoryUtrechtToZwolle = new Trajectory(stationUtrechtToZwolle,30,20,"train");
 
+
+
+
+      //Bus trajectories
+      //===========================================================================================================
       ArrayList<Station> utrechtToAmesfoort = new ArrayList();
-
       utrechtToAmesfoort.add(new Station("Utrecht CS",4 ,1.5));
       utrechtToAmesfoort.add(new Station("Vredenburg",1 ,2.4));
       utrechtToAmesfoort.add(new Station("Neude",1 ,0.3));
@@ -33,7 +54,6 @@ public class TrajectoryHandler {
 
 
       ArrayList<Station> utrechtToZeist = new ArrayList();
-
       utrechtToZeist.add(new Station("Utrecht CS",4 ,1.5));
       utrechtToZeist.add(new Station("Neude",1 ,0.3));
       utrechtToZeist.add(new Station("Janskerkhof",1 ,1.0));
@@ -50,16 +70,15 @@ public class TrajectoryHandler {
       utrechtToZeist.add(new Station("Schaerweijdelaan",3 ,1.5));// end station
       utrechtToZeist.add(new Station("Steylaan",3 ,0.7));// end station
       utrechtToZeist.add(new Station("Bussation zeist",0 ,0));// end station
-      Trajectory trajectoryUtrechtToZeist = new Trajectory(utrechtToZeist,30,20);
+      Trajectory trajectoryUtrechtToZeist = new Trajectory(utrechtToZeist,30,20, "bus");
 
-      trajectoryList.add(trajectoryUtrechtToAmesfoort);
-
-
-
-
+      //Bus trajectories
       trajectoryList.add(trajectoryUtrechtToZeist);
       trajectoryList.add(trajectoryUtrechtToAmesfoort);
+
+      //Train trajectories
       trajectoryList.add(trajectoryUtrechtToAmsterdam);
+      trajectoryList.add(trajectoryUtrechtToZwolle);
 
       return trajectoryList;
    }
