d20ac0eb1d74bde7bf514096cf3a17e3eeb0a757
ThijmenGeurtsen <thijmen01g@gmail.com>
2021-05-07 17:38:30 +0200

    added a function that generates list for departure and arrival

--- src/RouteData.java
+++ src/RouteData.java
@@ -7,11 +7,11 @@ public class RouteData {
     private String locationB;
     private double distance;
     private LocalTime time;
-    private String vehicleIdentifier;
+    private String vehicleIdentifier = "train";
     private ArrayList<Trajectory> trajectoryList;
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
-    public RouteData(){
+    public RouteData() {
         String str = "00:00";
         time = LocalTime.parse(str, formatter);
 
@@ -21,14 +21,15 @@ public class RouteData {
     }
 
 
-
-    public void addMinutesTime(int minutes){
+    public void addMinutesTime(int minutes) {
         time = time.plusMinutes(minutes);
     }
-    public LocalTime getTime(){
+
+    public LocalTime getTime() {
         return time;
     }
-    public void resetTime(){
+
+    public void resetTime() {
         time = LocalTime.parse("00:00", formatter);
 
 
@@ -37,74 +38,99 @@ public class RouteData {
     public String getLocationA() {
         return locationA;
     }
-    public void setLocationA(String locationA){
+
+    public void setLocationA(String locationA) {
         this.locationA = locationA;
     }
 
     public String getLocationB() {
         return locationB;
     }
-    public void setLocationB(String locationB){
+
+    public void setLocationB(String locationB) {
         this.locationB = locationB;
     }
 
     public double getDistance() {
         return distance;
     }
-    public void setDistance(double distance){
+
+    public void setDistance(double distance) {
         this.distance = distance;
     }
 
     public String getVehicleIdentifier() {
         return vehicleIdentifier;
     }
-    public void setVehicleIdentifier(String vehicleIdentifier){
+
+    public void setVehicleIdentifier(String vehicleIdentifier) {
         this.vehicleIdentifier = vehicleIdentifier;
     }
 
 
-    public ArrayList<Trajectory> getTrajectoryList(int index){
+    public ArrayList<Trajectory> getTrajectoryList(int index) {
         return trajectoryList;
     }
-    public void setTrajectoryList(ArrayList<Trajectory> trajectoryList){
+
+    public void setTrajectoryList(ArrayList<Trajectory> trajectoryList) {
         this.trajectoryList = trajectoryList;
     }
 
-    public ArrayList<Trajectory> getTrajectorysWithVehicleIdentifier(){
+    public ArrayList<Trajectory> getTrajectorysWithVehicleIdentifier() {
         ArrayList<Trajectory> newTrajectoryList = new ArrayList<>();
-        for (Trajectory trajectory : trajectoryList){
-            if (trajectory.getVehicleIdentifier().equals(getVehicleIdentifier())){
+        for (Trajectory trajectory : trajectoryList) {
+            if (trajectory.getVehicleIdentifier().equals(getVehicleIdentifier())) {
                 newTrajectoryList.add(trajectory);
             }
         }
         return newTrajectoryList;
     }
-    public ArrayList<String> getPossibleDepartureStation(){
-        ArrayList<String> departureStations = new ArrayList<>();
-        for (Trajectory trajectory : trajectoryList){
-            if (trajectory.getVehicleIdentifier().equals(getVehicleIdentifier())){
-                for (int i = 0;i < (trajectory.getStationList().size()-1); i++){
-                    String stationName = trajectory.getStationName(i);
-                    departureStations.add(stationName);
-                }
-            }
 
-        }
-        return departureStations;
-    }
 
-    public ArrayList<String> getPossibleArrivalStation(){
+    public ArrayList<String> getPossibleDepartureStation(String arrivalStation) {
         ArrayList<String> arrivalStations = new ArrayList<>();
-        for (Trajectory trajectory : trajectoryList){
-            if (trajectory.getVehicleIdentifier().equals(getVehicleIdentifier())){
-                for (int i = 1;i < (trajectory.getStationList().size()); i++){
+        arrivalStations.add("kies start locatie");
+        for (Trajectory trajectory : trajectoryList) {
+            if (trajectory.getVehicleIdentifier().equals(getVehicleIdentifier())) {
+                if (arrivalStation == null) {
+
+                } else if (trajectory.isStationInTrajectory(arrivalStation) == false) {
+                    continue;
+
+                }
+                for (int i = 0; i < (trajectory.getStationList().size() - 1); i++) {
                     String stationName = trajectory.getStationName(i);
                     arrivalStations.add(stationName);
+
                 }
             }
-
         }
         return arrivalStations;
     }
 
+    public ArrayList<String> getPossibleArrivalStation(String departureStation) {
+        ArrayList<String> arrivalStations = new ArrayList<>();
+        arrivalStations.add("kies eind locatie");
+
+        for (Trajectory trajectory : trajectoryList) {
+            if (trajectory.getVehicleIdentifier().equals(getVehicleIdentifier())) {
+
+
+                if (departureStation == null) {
+
+
+                } else if (trajectory.isStationInTrajectory(departureStation) == false) {
+                    continue;
+
+                }
+                for (int i = 1; i < (trajectory.getStationList().size()); i++) {
+                    String stationName = trajectory.getStationName(i);
+                    arrivalStations.add(stationName);
+
+                }
+            }
+        }
+        return arrivalStations;
+
+    }
 }
