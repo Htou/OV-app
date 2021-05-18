a9f1d35ad224cd19916b6e9686a6d43167e70db6
ThijmenGeurtsen <thijmen01g@gmail.com>
2021-05-18 13:49:07 +0200

    This hold the data for the profiles

---
+++ src/ProfileData.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ProfileData {
+    private ArrayList<Profile> profiles;
+
+
+
+    public ProfileData(ArrayList<Profile> profiles){
+        this.profiles = profiles;
+    }
+
+
+    public ArrayList<Profile> getProfiles(){
+        return this.profiles;
+    }
+}
+
