20bd6c812f9c551fc277abfdbc173a7135a60a38
htou <h.touati@tutanota.com>
2021-05-26 16:31:52 +0200

    Refactored class and moved functions with other uses away

---
+++ src/ProfileList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ProfileList {
+    private ArrayList<Profile> profiles;
+
+
+
+    public ProfileList(ArrayList<Profile> profiles){
+        this.profiles = profiles;
+    }
+
+    public ArrayList<Profile> getProfiles(){
+        return this.profiles;
+    }
+
+}
+
