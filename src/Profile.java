319c583b0672aa652dcc44a51ce851b79c6d356c
htou <h.touati@tutanota.com>
2021-05-31 16:10:14 +0200

    Added TravelHistory to Profile Class

--- src/Profile.java
+++ src/Profile.java
@@ -1,17 +1,26 @@
 public class Profile {
     private String name;
     private String password;
+    private TravelHistoryList travelHistorylist;
 
-    public Profile(String name, String password){
+    public Profile(String name, String password) {
         this.name = name;
         this.password = password;
+        this.travelHistorylist = new TravelHistoryList();
     }
 
-    public String getName(){
-        return name;
+    public String getName() {
+        return this.name;
     }
-    public String getPassword(){
-        return password;
+
+    public String getPassword() {
+        return this.password;
     }
+
+    public TravelHistoryList getTravelHistory() {
+        return this.travelHistorylist;
+    }
+
+
 }
 
