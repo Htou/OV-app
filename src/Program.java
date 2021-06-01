14b265c3babce0fd01f356cb0cf47b7808079d68
Hichem Touati <hichemtouati@outlook.com>
2021-06-01 21:33:16 +0200

    Created a temporary function call for creating profiles in profileData.json

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,9 @@
+import java.io.IOException;
+
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        DataHandler.createUserProfile();
 
         Gui myLayout = new Gui();
         myLayout.setVisible(true);
