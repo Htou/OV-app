d3bc9e8fc8ce68b6256c7b1eb666766672fd41f8
Hichem Touati <hichemtouati@outlook.com>
2021-06-02 11:40:16 +0200

    Removed json-lib to use Gson

--- src/Profile.java
+++ src/Profile.java
@@ -26,36 +26,5 @@ public class Profile {
         return this.travelHistorylist;
     }
 
-    //Convert a profile instance to a JSONObject
-    public JSONObject toJSON() {
-        JSONObject profileJSON = new JSONObject();
-        profileJSON.put("name", name);
-        profileJSON.put("password", password);
-        profileJSON.put("travelHistoryList", travelHistorylist.getTravelHistoryList().get(0));
-        return profileJSON;
-    }
-
-    //set JSONObject to javacode(String)
-    public Profile(JSONObject profileJSON) {
-        name = profileJSON.getString("name");
-        password = profileJSON.getString("password");
-
-        JSONArray arr = profileJSON.getJSONArray("travelHistoryList");
-        for (int i = 0; i < arr.length(); i++) {
-            JSONObject historyList = arr.getJSONObject(i);
-            System.out.println(historyList);
-        }
-
-    }
-
-
-
-
-    //print name and password
-    public void write() {
-        System.out.print("name=" + name);
-        System.out.print("password=" + password);
-    }
-
 }
 
