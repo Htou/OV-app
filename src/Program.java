590381bb66c6162a3d41ae3b66e7d34e78727d8b
Hichem Touati <hichemtouati@outlook.com>
2021-06-02 18:20:28 +0200

    Commented out create userProfile Functionality because we dont need it right now

--- src/Program.java
+++ src/Program.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Program {
 
     public static void main(String[] args) throws IOException {
-        DataHandler.createUserProfile();
+//        DataHandler.createUserProfile();
 
         Gui myLayout = new Gui();
         myLayout.setVisible(true);
